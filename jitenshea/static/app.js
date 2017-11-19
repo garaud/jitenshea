b79067c9591c0ff817ad6bb82cff83ea7a70d7a1
Damien Garaud <damien.garaud@gmail.com>
2017-11-19 21:05:17 +0100

    js: fix a function name

--- jitenshea/static/app.js
+++ jitenshea/static/app.js
@@ -4,6 +4,6 @@ var API_URL = "http://jitenshea.dev/api"
 
 // Build the URL with a BASE_URL/<city> suffix based from a DOM element with the
 // "city" attribute.
-function url(dom_id) {
+function cityurl(dom_id) {
   return API_URL + "/" + document.getElementById(dom_id).getAttribute("city");
 };
