150fbe531503a0aa363c7ce348c57fbb0ddbd1ff
Damien Garaud <damien.garaud@gmail.com>
2017-11-20 00:39:39 +0100

    ui: short summary for the station page

--- jitenshea/static/station.js
+++ jitenshea/static/station.js
@@ -1,2 +1,15 @@
 // Jitenshea functions for the 'station' page
 
+
+// Station summary
+$(document).ready(function() {
+  var station_id = document.getElementById("stationSummary").getAttribute("station_id");
+  $.get(cityurl("stationSummary") + "/station/" + station_id, function (content) {
+    var station = content.data[0];
+    $("#id").append(station.id);
+    $("#name").append(station.name);
+    $("#nbBikes").append(station.nb_bikes);
+    $("#address").append(station.address);
+    $("#city").append(station.city);
+  } );
+} );
