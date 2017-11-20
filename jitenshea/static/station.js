54c44f37db28fe0288b92e2767bd825348d56186
Damien Garaud <damien.garaud@gmail.com>
2017-11-20 01:54:21 +0100

    ui: plot the timeseries in the station page

--- jitenshea/static/station.js
+++ jitenshea/static/station.js
@@ -14,3 +14,49 @@ $(document).ready(function() {
     $("#city").append(station.city);
   } );
 } );
+
+
+
+// Timeseries plot
+$(document).ready(function() {
+  var station_id = document.getElementById("stationTimeseries").getAttribute("station_id");
+  // Only plot two days.
+  var start = '2017-07-29'
+  var stop = '2017-07-31';
+  // day before today
+  // var yesterday = new Date()
+  // yesterday.setDate(yesterday.getDate() - 1);
+  // console.log(yesterday.toISOString().substring(0, 10));
+  var url = cityurl("stationTimeseries") + "/timeseries/station/" + station_id
+      + "?start=" + start + "&stop=" + stop;
+  $.get(url, function(content) {
+    var station_name = content.data[0].name;
+    var date = content.data[0].ts;
+    var stands = date.map(function(t, i) {
+      return [Date.parse(t), content.data[0].available_stand[i]];
+    });
+    var bikes = date.map(function(t, i) {
+      return [Date.parse(t), content.data[0].available_bike[i]];
+    });
+    Highcharts.chart('stationTimeseries', {
+      title: {
+        text: 'Station ' + station_name
+      },
+      yAxis: {
+        title: {
+          text: 'Available bikes and stands'
+        }
+      },
+      xAxis: {
+        type: "datetime"
+      },
+      series: [{
+        "name": "stands",
+        "data": stands
+      }, {
+        "name": "bikes",
+        "data": bikes
+      }]
+    } );
+  } );
+} );
