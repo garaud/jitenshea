36e028af03abf420875d63a7a53793b5cb9a0466
Damien Garaud <damien.garaud@gmail.com>
2017-11-24 18:32:10 +0100

    ui: city.js refactoring
    Make a specific function to set the map of all stations for a specific city

--- jitenshea/static/city.js
+++ jitenshea/static/city.js
@@ -3,6 +3,9 @@
 // Note : the 'cityurl' function is in the 'app.js' file.
 
 // List of stations with DataTables
+// Store the list of station in a sessionStorage.
+// The stations list item will be used for each station page (a tiny
+// station-centered map)
 $(document).ready(function() {
   var city = document.getElementById("citytable").getAttribute("city");
   $('#citytable').DataTable( {
@@ -11,7 +14,7 @@ $(document).ready(function() {
     paging:         false,
     processing: true,
     ajax: {
-      url: cityurl("citytable") + "/station?limit=400"
+      url: cityurl("citytable") + "/station?limit=600"
     },
     columnDefs: [ {
       "targets": 1,
@@ -31,35 +34,39 @@ $(document).ready(function() {
 } );
 
 
+function stationsMap(map, data) {
+  var OSM_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  });
+  OSM_Mapnik.addTo(map);
+  var centroid = turf.center(data);
+  map.setView([centroid.geometry.coordinates[1],
+               centroid.geometry.coordinates[0]], 12);
+  L.geoJSON(data, {
+    pointToLayer: function(geoJsonPoint, latlng) {
+      return L.circleMarker(latlng, {radius: 5})
+        .bindPopup("<ul><li><b>ID</b>: " + geoJsonPoint.properties.id
+                   + "</li><li><b>Name</b>: " + geoJsonPoint.properties.name + "</li></ul>")
+        .on('mouseover', function(e) {
+          this.openPopup();
+        })
+        .on('mouseout', function(e) {
+          this.closePopup();
+        });
+    }
+  }).addTo(map);
+};
+
 // Map with all stations with Leaflet
 // TODO :
 //  - is it possible to set a bbox (computed by turjs) instead of a zoom in the
 //    'setView' function.
 $(document).ready(function() {
   var station_map = L.map("stationMap");
-  var OSM_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  });
-  OSM_Mapnik.addTo(station_map);
-  $.get(cityurl("stationMap") + "/station?geojson=true&limit=400", function(data) {
-    // Get the centroid of all stations.
-    var centroid = turf.center(data);
-    station_map.setView([centroid.geometry.coordinates[1],
-                         centroid.geometry.coordinates[0]], 12);
-    L.geoJSON(data, {
-      pointToLayer: function(geoJsonPoint, latlng) {
-        return L.circleMarker(latlng, {radius: 5})
-          .bindPopup("<ul><li><b>ID</b>: " + geoJsonPoint.properties.id
-                     + "</li><li><b>Name</b>: " + geoJsonPoint.properties.name + "</li></ul>")
-          .on('mouseover', function(e) {
-            this.openPopup();
-          })
-          .on('mouseout', function(e) {
-            this.closePopup();
-          });
-      }
-    }).addTo(station_map);
+  var city = document.getElementById("stationMap").getAttribute("city");
+  $.get(cityurl("stationMap") + "/station?geojson=true&limit=600", function(data) {
+    stationsMap(station_map, data);
   } );
 } );
 
