9e72312a01f2cf107fd64a85afb955121a0e1e08
Damien Garaud <damien.garaud@gmail.com>
2017-11-24 19:10:34 +0100

    ui: add a map to the station page
    * a specific marker for the station
    * a map station-centered

--- jitenshea/static/station.js
+++ jitenshea/static/station.js
@@ -16,6 +16,66 @@ $(document).ready(function() {
 } );
 
 
+// Get the station ID from the URL /<city>/ID
+function stationIdFromURL() {
+  var path = window.location.pathname;
+  var elements = path.split('/')
+  return parseInt(elements[elements.length -1]);
+}
+
+// Map centered to the station
+function stationsMap(map, data) {
+  var OSM_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  });
+  OSM_Mapnik.addTo(map);
+  // The center of the map is the station coords
+  var station_id = stationIdFromURL();
+  var station = data.features.filter(feature => feature.properties.id == station_id);
+  map.setView([station[0].geometry.coordinates[1],
+               station[0].geometry.coordinates[0]], 16);
+  L.geoJSON(data, {
+    pointToLayer: function(geoJsonPoint, latlng) {
+      var marker = null;
+      if (geoJsonPoint.properties.id == station_id) {
+        marker = L.marker(latlng);
+      } else {
+        marker = L.circleMarker(latlng, {radius: 5});
+      }
+      // Popup with ID and name.
+      marker.bindPopup("<ul><li><b>ID</b>: " + geoJsonPoint.properties.id
+                       + "</li><li><b>Name</b>: " + geoJsonPoint.properties.name + "</li></ul>")
+        .on('mouseover', function(e) {
+          this.openPopup();
+        })
+        .on('mouseout', function(e) {
+          this.closePopup();
+        });
+      return marker;
+    }
+  }).addTo(map);
+};
+
+
+// Map centered to the station with Leaflet
+$(document).ready(function() {
+  var station_map = L.map("stationMap");
+  var city = document.getElementById("stationMap").getAttribute("city");
+  var geostations = sessionStorage.getItem(city);
+  if (geostations == null) {
+    $.get(cityurl("stationMap") + "/station?geojson=true&limit=600", function(data) {
+      console.log("stations geodata GET request in " + city);
+      stationsMap(station_map, data);
+      sessionStorage.setItem(city, JSON.stringify(data));
+    } );
+  } else {
+    console.log("station geodata from sesssionStorage in " + city);
+    stationsMap(station_map, JSON.parse(geostations));
+  }
+} );
+
+
 
 // Timeseries plot
 $(document).ready(function() {
