486803b476e32b4b9a835a202c0f2314f25f24cc
Damien Garaud <damien.garaud@gmail.com>
2018-04-07 09:59:24 +0200

    update the psycopg2 dependency in the setup.py
    Avoid a warning message about the psycopg2-binary package

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ import setuptools
 with open("README.md") as fobj:
     LONG_DESCRIPTION = fobj.read()
 
-INSTALL_REQUIRES = ["pandas", "requests", "psycopg2", "luigi", 'sqlalchemy', 'lxml',
+INSTALL_REQUIRES = ["pandas", "requests", "psycopg2-binary", "luigi", 'sqlalchemy', 'lxml',
                     'daiquiri', 'flask-restplus', 'sh', 'scikit-learn', 'tables']
+
 
 setuptools.setup(
     name='jitenshea',
