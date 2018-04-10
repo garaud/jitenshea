cc587d50c4b29883b5d348c5fad940d90ad1ae78
Damien Garaud <damien.garaud@gmail.com>
2018-04-10 18:06:54 +0200

    data: create a sub-directory to store clustering results
    Also call the 'makedirs()' method for the LocalTarget in order to create the
    sub-directories since you don't use a context manager.

--- jitenshea/tasks/bordeaux.py
+++ jitenshea/tasks/bordeaux.py
@@ -364,10 +364,8 @@ class BordeauxComputeClusters(luigi.Task):
     stop = luigi.DateParameter(default=date.today())
 
     def outputpath(self):
-        start_date = self.start.strftime("%Y%m%d")
-        stop_date = self.stop.strftime("%Y%m%d")
-        fname = "bordeaux-{}-{}-clustering.h5".format(start_date, stop_date)
-        return os.path.join(DATADIR, fname)
+        fname = "kmeans-{}-to-{}.h5".format(self.start, self.stop)
+        return os.path.join(DATADIR, 'clustering', fname)
 
     def output(self):
         return luigi.LocalTarget(self.outputpath(), format=MixedUnicodeBytes)
@@ -384,6 +382,7 @@ class BordeauxComputeClusters(luigi.Task):
                                               "stop": self.stop})
         df.columns = ["station_id", "ts", "nb_bikes"]
         clusters = compute_clusters(df)
+        self.output().makedirs()
         path = self.output().path
         clusters['labels'].to_hdf(path, '/clusters')
         clusters['centroids'].to_hdf(path, '/centroids')
